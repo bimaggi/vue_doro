9ccf9c713fdd9de49c1ca014a3e359bf4e30330a
bimaggi <bmg.olivier@gmail.com>
2021-10-25 17:21:30 -0300

    feat(footer):add footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+    <p>
+        <a href="https://www.linkedin.com/in/marcia-bimaggi-marcia-beserra-de-oliveira-9303801b9/"
+          class="footer_link"
+          target="_blank">
+        <strong>
+          Marcia Bimaggi - 2021 - Projeto desenvolvido no Curso VueJs na pratica - Prof Igor Souto
+        </strong>
+      </a>
+    </p>
+</footer>
+</template>
+<style lang="stylus" scoped>
+ footer
+  text-align: center
+.footer_link
+  letter-spacing: 1px
+  text-decoration: none
+</style>
